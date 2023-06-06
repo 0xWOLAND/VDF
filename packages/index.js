dc2dc8f183cab1418c860607bada5c20c30b6e7e
chndr1 <bhargav.annem@gmail.com>
2023-06-06 15:29:02 -0500

    feat: drand beacon

---
+++ packages/index.js
@@ -0,0 +1,5 @@
+import { beacon } from "./utils/beacon.mjs";
+
+const tmp = await beacon();
+console.log("test");
+console.log(tmp);
---
+++ packages/utils/beacon.mjs
@@ -0,0 +1,23 @@
+import fetch from "node-fetch";
+import AbortController from "abort-controller";
+import { fetchBeacon, HttpChainClient, HttpCachingChain } from "drand-client";
+
+global.fetch = fetch;
+global.AbortController = AbortController;
+
+const chainHash =
+  "8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce"; // (hex encoded)
+const publicKey =
+  "868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31"; // (hex encoded)
+
+export const beacon = async () => {
+  const options = {
+    disableBeaconVerification: false,
+    noCache: false,
+    chainVerificationParams: { chainHash, publicKey },
+  };
+
+  const chain = new HttpCachingChain("https://api.drand.sh", options);
+  const client = new HttpChainClient(chain, options);
+  return await fetchBeacon(client);
+};
