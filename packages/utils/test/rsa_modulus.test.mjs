cb90af2000bc62fc9c6839994401721224098440
Sarthak Sarans <sarthaksarans@gmail.com>
2023-06-07 15:10:52 -0500

    added docusarus to packages file

--- packages/utils/test/rsa_modulus.test.mjs
+++ packages/utils/test/rsa_modulus.test.mjs
@@ -3,11 +3,29 @@ import { prime } from "../src/rsa_modulus.mjs";
 
 describe("test", () => {
   it("test", () => {
-    Array.from(Array(100).key()).forEach((x) => {
+    Array.from(Array(100).keys()).forEach((x) => {
       const isPrime = prime(x);
       // sieve
-      const isPrimeFromSieve = false;
+      let isPrimeFromSieve = false;
+
+      let primeTest = new Array(x + 1).fill(true);
+      primeTest[0] = primeTest[1] = false;
+
+      for (let p = 2; p * p <= x; p++) {
+        // If primeTest[p] is not changed, then it is a prime
+        if (primeTest[p]) {
+          // Update all multiples of p
+          for (let i = p * p; i <= x; i += p) {
+            primeTest[i] = false;
+          }
+        }
+      }
+      isPrimeFromSieve = primeTest[x];
       assert.equal(isPrime, isPrimeFromSieve);
     });
   });
+
+  // it("better", () => {
+  //   assert.equal(1,1);
+  // });
 });
